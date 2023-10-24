7433839872117ae09fdcf6b153947a5c2e242eb2
guysbeing <132474410+guysbeing@users.noreply.github.com>
2023-10-24 19:32:47 +0800

    鑷姩鎻愪氦

--- commit.bat
+++ commit.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM 导航到本地仓库路径
-cd "D:\桌面\DOWNLOAD\demo"
+cd /d "D:\桌面\DOWNLOAD\demo"
 
 REM 添加更改
 git add .
@@ -10,4 +10,4 @@ REM 提交更改
 git commit -m "自动提交"
 
 REM 推送到远程仓库
-git push origin master
+git push origin main
